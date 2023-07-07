d6bbf3a589b265d9901873addafd04f7bbb69114
Susmitaa-Das <susmitadas2003hasi2021@gmail.com>
2023-07-07 20:35:19 +0530

    Added Mongoose in package json file

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import express from 'express'
 import path from 'path'
+import mongoose from 'mongoose'
+
+mongoose
+  .connect('mongodb://127.0.0.1:27017', {
+    dbName: 'backend',
+  })
+  .then(() => console.log('Database Connected'))
+  .catch((e) => console.log(e))
 
 const app = express()
 
@@ -14,6 +22,10 @@ app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   res.render('index', { name: 'chayan' })
+})
+
+app.get('/add', (req, res) => {
+  res.send('Nice')
 })
 
 app.get('/success', (req, res) => {
