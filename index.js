106e463e59a82f71e726be5408bdfecdd21c2d02
Susmitaa-Das <susmitadas2003hasi2021@gmail.com>
2023-07-14 00:29:31 +0530

    Updated index js file and login js file. Added jsonwebtoken dependencies in the package json file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import path from 'path'
 import mongoose from 'mongoose'
 import cookieParser from 'cookie-parser'
+import jwt from 'jsonwebtoken'
 
 mongoose
   .connect('mongodb://127.0.0.1:27017', {
@@ -27,10 +28,12 @@ app.use(cookieParser())
 //Setting up view engine
 app.set('view engine', 'ejs')
 
-const isAuthenticated = (req, res, next) => {
+const isAuthenticated = async (req, res, next) => {
   const { token } = req.cookies
 
   if (token) {
+    const decoded = jwt.verify(token, 'sdjfrjgrjgjgjghfdg')
+    req.user = await User.findById(decoded._id)
     next()
   } else {
     res.render('login')
@@ -38,11 +41,20 @@ const isAuthenticated = (req, res, next) => {
 }
 
 app.get('/', isAuthenticated, (req, res) => {
+  console.log(req.user)
   res.render('logout')
 })
 
-app.post('/login', (req, res) => {
-  res.cookie('token', 'iamin', {
+app.post('/login', async (req, res) => {
+  const { name, email } = req.body
+  const user = await User.create({
+    name,
+    email,
+  })
+
+  const token = jwt.sign({ _id: user._id }, 'sdjfrjgrjgjgjghfdg')
+
+  res.cookie('token', token, {
     httpOnly: true,
     expires: new Date(Date.now() + 60 * 1000),
   })
